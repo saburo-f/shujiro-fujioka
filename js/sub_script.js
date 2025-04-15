e2d4f9cf667baf8d8b65630d0059a01dc2869abd
saburo-f <s.fujioka705@gmail.com>
2025-04-15 11:13:08 +0900

    script

---
+++ js/sub_script.js
@@ -0,0 +1,39 @@
+{
+  "redirects": [
+    {
+      "source": "/index.html",
+      "destination": "/",
+      "permanent": true
+    },
+    {
+      "source": "/profile.html",
+      "destination": "/profile",
+      "permanent": true
+    },
+    {
+      "source": "/publication.html",
+      "destination": "/publication",
+      "permanent": true
+    },
+    {
+      "source": "/contact.html",
+      "destination": "/contact",
+      "permanent": true
+    }
+  ],
+  "rewrites": [
+    {
+      "source": "/profile",
+      "destination": "/profile.html"
+    },
+    {
+      "source": "/publication",
+      "destination": "/publication.html"
+    },
+    {
+      "source": "/contact",
+      "destination": "/contact.html"
+    }
+  ]
+}
+
