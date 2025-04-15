bbe6296784a7576999353831487696edcaf642b6
saburo-f <s.fujioka705@gmail.com>
2025-04-15 11:03:19 +0900

    html

--- script.js
+++ script.js
@@ -6,3 +6,42 @@ document.addEventListener('DOMContentLoaded', function() {
     menuItems.classList.toggle('show');
   });
 });
+
+{
+  "redirects"; [
+    {
+      "source": "/index.html",
+      "destination": "/",
+      "permanent": true
+    },
+    {
+      "source": "/profile.html",
+      "destination": "/profile",
+      "permanent": true
+    },
+    {
+      "source": "/publication.html",
+      "destination": "/publication",
+      "permanent": true
+    },
+    {
+      "source": "/contact.html",
+      "destination": "/contact",
+      "permanent": true
+    }
+  ],
+  "rewrites"; [
+    {
+      "source": "/profile",
+      "destination": "/profile.html"
+    },
+    {
+      "source": "/publication",
+      "destination": "/publication.html"
+    },
+    {
+      "source": "/contact",
+      "destination": "/contact.html"
+    }
+  ]
+}
